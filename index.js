c3445b9e13542a0b451f5b926b442fd39963b653
zhangtao <zhangtao@yunkai.com>
2017-10-06 00:07:46 +0800

    update router

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-index.js
+var express = require('express');
+var app = express();
+var router = require('./routes.js');
+
+app.use('/', router);
+
+app.listen(8080, function() {
+	console.log('app server start listen on 8080');
+})
